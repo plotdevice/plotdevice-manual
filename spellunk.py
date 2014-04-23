1f00f6bd199fe43ed7b48b286dd76bc5837e2cca
Christian Swinehart <drafting@samizdat.cc>
2014-04-23 09:53:19 -0400

    snippet generator

--- spellunk.py
+++ spellunk.py
@@ -24,47 +24,15 @@ from pygments.token import Punctuation, Keyword, Name, Number, Text
 from jinja2 import Environment, FileSystemLoader
 from bs4 import BeautifulSoup, NavigableString
 
-def reorg():
-  ref=odict()
-  ref['Canvas'] = {
-      'commands': ['canvas()', 'speed()', 'background()', 'export()', 'clear()', 'plot()',],
-      'types': ['Constants'],
-      'compat': ['outputmode()', 'size()'] }
-  ref['Primitives'] = {
-      'commands': ['image()', 'rect()',  'oval()', 'poly()', 'line()', ],
-      'types': ['Image'],
-      'compat': ['arrow()', 'star()'] }
-  ref['Drawing'] = {
-      'commands': ['bezier()', 'moveto()', 'lineto()', 'arcto()', 'curveto()', ],
-      'types': ['Bezier', 'Curve'],
-      'compat': ['autoclosepath()', 'beginclip()', 'beginpath()', 'drawpath()', 'endclip()', 'endpath()', 'findpath()'] }
-  ref['Line+Color'] = {
-      'commands': ['plotstyle()', 'stroke()', 'fill()', 'pen()', 'color()'],
-      'types': ['Color', 'Gradient'],
-      'compat': ['capstyle()', 'colormode()', 'joinstyle()', 'nofill()', 'nostroke()', 'strokewidth()'] }
-  ref['Transform'] = {
-      'commands': ['transform()', 'translate()', 'rotate()', 'scale()', 'skew()', 'reset()', ],
-      'types': ['Transform'],
-      'compat': ['pop()', 'push()'] }
-  ref['Compositing'] = {
-      'commands': ['effects()','alpha()', 'blend()', 'shadow()', 'clip()', ],
-      'types': ['Shadow', ],
-      'compat': ['noshadow()',] }
-  ref['Typography'] = {
-      'commands': ['font()', 'text()', 'align()', 'stylesheet()'],
-      'types': ['Family', 'Font', 'Stylesheet', 'Text'],
-      'compat': ['fontsize()', 'lineheight()', 'textheight()', 'textmetrics()', 'textpath()', 'textwidth()'] }
-  ref['Utility'] = {
-      'commands': ['read()', 'grid()', 'measure()', 'files()', 'fonts()', 'random()', 'choice()', 'shuffled()', 'ordered()'],
-      'types': ['Point', 'Size', 'Region', 'dictionaries', ],
-      'compat': ['imagesize()', 'autotext()', 'open()'] }
+sys.path.append('%s/src'%py_root) # so we can pick up toc.py
+import toc
 
-  tut=odict()
-  tut['Basics']=["Introduction", "Environment", "Primitives", "Graphics_State",]
-  tut['Specifics']=["Animation", u"Bézier Paths", "Color", "Math",]
-  tut['Data']=["Variables", "Lists", "Strings", "Serialization"]
-  tut['Structure']=["Repetition", "Commands", "Libraries", "Classes",]
-  tut['Advanced']=["Interaction", "Extending", "Scripting", "plotdevice",]
+def _flatten(seq):
+    return sum( ([x] if not isinstance(x, (list,tuple)) else list(x) for x in seq), [] )
+
+def reorg():
+  ref = toc.ref
+  tut = toc.tut
 
   extant = {}
   for pth in glob('src/ref/*/*/*.html'):
@@ -74,6 +42,7 @@ def reorg():
   preferred = {}
   for sect, groups in ref.items():
     for group, calls in groups.items():
+      calls = _flatten(calls)
       for api in calls:
         preferred[api] = (sect, group)
 
@@ -88,11 +57,11 @@ def reorg():
     if old!= new:
       print api, old,'->',new
 
-  oldbook=["Introduction", "Environment", "Primitives", "Graphics_State", "Variables", "Lists", "Strings", "Repetition", "Commands", "Libraries", "Classes", "Animation", "Interaction", "Paths", "Color", "Math", "Extending", "Scripting", "plotdevice"]
-  newbook = sum(tut.values(), [])
-  for i in xrange(len(newbook)):
-    print '%2i'%(i+1), oldbook[i] if oldbook[i:] else '<>', newbook[i].encode('utf-8')
-  print newbook
+  # oldbook=["Introduction", "Environment", "Primitives", "Graphics_State", "Variables", "Lists", "Strings", "Repetition", "Commands", "Libraries", "Classes", "Animation", "Interaction", "Paths", "Color", "Math", "Extending", "Scripting", "plotdevice"]
+  # newbook = sum(tut.values(), [])
+  # for i in xrange(len(newbook)):
+  #   print '%2i'%(i+1), oldbook[i] if oldbook[i:] else '<>', newbook[i].encode('utf-8')
+  # print newbook
 
 if __name__=='__main__':
   reorg()
