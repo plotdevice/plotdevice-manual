af79db76cfcc5135e9e2b5acb3a253919e1ceca8
Christian Swinehart <drafting@samizdat.cc>
2014-11-19 17:45:12 -0500

    initial docs for the new Text* classes

--- src/toc.py
+++ src/toc.py
@@ -27,7 +27,7 @@ ref['Compositing'] = {
     'compat': ['noshadow()', 'beginclip()', 'endclip()', ] }
 ref['Typography'] = {
     'commands': ['text()', 'textpath()', 'paginate()', 'stylesheet()', 'font()'],
-    'types': ['Text', 'TextFrame', 'Family', 'Font', ],
+    'types': ['Text', 'TextFrame', 'TextMatch', 'LineFragment', ('Font', 'Family'), ],
     'compat': ['align()', 'fontsize()', 'lineheight()', 'textheight()', 'textmetrics()', 'textwidth()'] }
 # grouped into Misc.html
 ref['Utility'] = {
